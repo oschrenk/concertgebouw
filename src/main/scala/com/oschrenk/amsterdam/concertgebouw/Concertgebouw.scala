bab60f0363b0b270c8661a79672bbd0d84b2199e
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-02-25 22:19:23 +0100

    Convert to iCal

--- src/main/scala/com/oschrenk/amsterdam/concertgebouw/Concertgebouw.scala
+++ src/main/scala/com/oschrenk/amsterdam/concertgebouw/Concertgebouw.scala
@@ -1,15 +1,21 @@
 package com.oschrenk.amsterdam.concertgebouw
 
 import cats.effect.IO
+import java.time._
+import ical.Event
 import org.http4s.client.{Client, blaze}
 
 object Concertgebouw extends App {
 
-  val client: Client[IO] = blaze.Http1Client[IO]().unsafeRunSync()
+  private val client: Client[IO] = blaze.Http1Client[IO]().unsafeRunSync()
+  private val zoneId = ZoneId.of("Europe/Amsterdam")
 
-  new Network(client).lunchtimeConcerts().unsafeRunSync().foreach( concert =>
-    println(concert)
-  )
+  new Network(client).lunchtimeConcerts().unsafeRunSync().foreach{ concert =>
+    val start = concert.event_start_date.atZone(zoneId)
+    val end = concert.event_end_date.atZone(zoneId)
+    val event = Event.from(concert.title, start, end, None)
+    println(event)
+  }
 
   client.shutdownNow()
 }
